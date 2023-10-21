45f9182c531edd6a7e1bcb4b276f4cfee0b51caa
KsanaSal <oasalivon@gmail.com>
2023-10-21 22:50:28 +0300

    added library and services for work

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+import { UsersModule } from './Users/users.module';
+import { AuthModule } from './authentication/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, AuthModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const cors = {
+    origin: ['http://localhost:3000'],
+    method: 'GET, POST, HEAD, PUT, PATCH, DELETE, OPTIONS',
+  };
+  app.enableCors(cors);
   await app.listen(3000);
 }
 bootstrap();
