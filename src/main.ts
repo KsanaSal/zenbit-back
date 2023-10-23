a89b0bab00e898e8d5a463f72b375af525ef2f51
KsanaSal <oasalivon@gmail.com>
2023-10-23 17:43:13 +0300

    change cors

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const cors = {
-    origin: ['http://localhost:3001', 'https://zenbit-test.vercel.app/'],
-    method: 'GET, POST, HEAD, PUT, PATCH, DELETE, OPTIONS',
-  };
-  app.enableCors(cors);
+  // const cors = {
+  //   origin: ['http://localhost:3001', 'https://zenbit-test.vercel.app'],
+  //   method: 'GET, POST, HEAD, PUT, PATCH, DELETE, OPTIONS',
+  // };
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
